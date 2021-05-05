f7bc9c19016e1ee92f5d6f0f2e1d3bdca9b5758b
Julio Viegas <julio.viegas@sap.com>
2021-05-05 14:39:53 -0400

    create mystore-ssr

---
+++ js-storefront/mystore-ssr/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://spartacus-demo.eastus.cloudapp.azure.com:8443',
+          prefix: '/occ/v2/'
+        }
+      },
+      context: {
+        currency: ['USD'],
+        language: ['en'],
+        baseSite: ['electronics-spa']
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '2.0'
+      }
+    }),
+    BrowserTransferStateModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
